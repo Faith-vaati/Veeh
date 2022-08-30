ac2177024bac4c25f628f4feb90d078ccbef16e3
Faith_vaati <vaati.faith79@gmail.com>
2022-08-30 23:53:39 +0300

    menu

--- contact.php
+++ contact.php
@@ -5,12 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
 		
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="about">About</a>
-  <a href="contact">Contact</a>
-  <a href="career">Career</a>
-</div>
 
 	</title>
 </head>
