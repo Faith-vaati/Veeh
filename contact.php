bb557e527cf4119aff111df4f4e62e0def5a92a3
Faith_vaati <vaati.faith79@gmail.com>
2022-09-01 23:43:43 +0300

    contact page

--- contact.php
+++ contact.php
@@ -4,11 +4,21 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
-		
+	
+	Reach out to us	
 
 	</title>
 </head>
 <body>
-
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+  <label for="pno">Phone Number:</label><br>
+  <input type="text" id="pno" name="pno">
+  <label for="career">Career:</label><br>
+  <input type="text" id="career" name="career">
+</form>
 </body>
 </html>
