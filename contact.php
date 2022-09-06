e920b2315c79bb179c5f87d116f3449ef2765820
Faith_vaati <vaati.faith79@gmail.com>
2022-09-06 23:57:08 +0300

    contact edits

--- contact.php
+++ contact.php
@@ -13,6 +13,8 @@
 <form>
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname"><br>
+  <label for="mname">Middle name:</label><br>
+  <input type="text" id="mname" name="mname">
   <label for="lname">Last name:</label><br>
   <input type="text" id="lname" name="lname">
   <label for="pno">Phone Number:</label><br>
