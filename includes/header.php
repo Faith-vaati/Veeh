c1e93e72f1f5eaea85dfca76f53aef358d3d2e8f
Faith_vaati <vaati.faith79@gmail.com>
2022-09-09 15:08:21 +0300

    header

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,24 @@
 
 </head>
 
+<body>
+  <h2>
+    
+    <div style="width: 100%;background:gray;height: 50px;float: right;">
+      
+<?php
+  if($page=="hm"){echo "Home";}
+   if($page=="ab"){echo "About";}
+    if($page=="cr"){echo "Career";}
+     if($page=="ev"){echo "Events";}
+      if($page=="cts"){echo "Contact Us";}
+       
+?>
 
+    </div>
+  </h2>
+
+</body>
 
 
 
