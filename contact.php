09c8df4f1345b80433e6971f54dfa581fb453ab4
Faith_vaati <vaati.faith79@gmail.com>
2022-09-09 14:54:40 +0300

    menu

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ Welcome to Dashboard
 
 
 <br><br>
-<form action="add_member.php?pg=mem" method="post" enctype="multipart/form-data">
+<form action="contact.php?pg=cnt" method="post" enctype="multipart/form-data">
 
 <table width="80%" border="0" cellspacing="0">
   <tr><td>Full name:</td><td><input type="text" name="fname" placeholder="Type First Input" required></td></tr>
@@ -15,10 +15,8 @@ Welcome to Dashboard
      <tr><td>Address:</td><td><textarea name="addr"></textarea></td></tr>
      <tr><td>Email:</td><td><input type="email" name="email" required></td></tr>
      <tr><td>Mobile no.:</td><td><input type="text" name="mobno"></td></tr>
+     <tr><td>Address:</td><td><textarea name="addr"></textarea></td></tr>
      <tr><td>Profile photo:</td><td><input type="file" name="photo"?></td></tr>
-      <tr><td>Username:</td><td><input type="text" name="user" required?></td></tr>
-     <tr><td>Password:</td><td><input type="password" name="pass"></td></tr>
-     <tr><td>Confirm password:</td><td><input type="password" name="conpass"></td></tr>
      <tr><td colspan="2"><input type ="submit" name="sub" value="Register"></td></tr>
 
 </table>
--- includes/extra.php
+++ includes/extra.php
@@ -1,14 +1,56 @@
-<!DOCTYPE html>
-<html>
+
+<!-- menu -->
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>
-	
-	</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+    
+<div class="topnav">
+  <a class="active" href="index.php">Veeh</a>
+  <a href="home.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="contact">Contact</a>
+  <a href="career">Career</a>
+</div>
+
+<style type="text/css">
+  
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+  
+</style>
+
+  </title>
 </head>
 <body>
-<!-- Container for the image gallery -->
+<h1>
+  <!-- Container for the image gallery -->
 <div class="container">
 
   <!-- Full-width images with number text -->
@@ -76,9 +118,9 @@
 
 
 <style type="text/css">
-	
+  
 
-	* {
+  * {
   box-sizing: border-box;
 }
 
@@ -167,8 +209,8 @@
 </style>
 
 <script type="text/javascript">
-	
-	let slideIndex = 1;
+  
+  let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -199,61 +241,11 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 </script>
+</h1>
 </body>
 </html>
 
 
-<!-- menu -->
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>
-    
-<div class="topnav">
-  <a class="active" href="index.php">Veeh</a>
-  <a href="home.php">Home</a>
-  <a href="about.php">About</a>
-  <a href="contact">Contact</a>
-  <a href="career">Career</a>
-</div>
 
-<style type="text/css">
-  
-/* Add a black background color to the top navigation */
-.topnav {
-  background-color: #333;
-  overflow: hidden;
-}
 
-/* Style the links inside the navigation bar */
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
 
-/* Change the color of links on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Add a color to the active/current link */
-.topnav a.active {
-  background-color: #04AA6D;
-  color: white;
-}
-  
-</style>
-
-  </title>
-</head>
-<body>
-
-</body>
-</html>
